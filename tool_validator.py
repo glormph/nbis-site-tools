0034358c29f6fada105a2ac28743e13709ace224
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2017-01-23 09:52:52 +0100

    Tool YML validation and creation script, and a toolstub JSON to feed the script.

---
+++ tool_validator.py
@@ -0,0 +1,104 @@
+import json
+import urllib2
+import yaml
+import sys
+import bibtexparser
+
+
+BIO_TOOLS_URL = 'https://bio.tools/api/tool/{}/version/{}'
+DOI_URL = 'http://dx.doi.org/{}'
+
+# Which fields are required:
+fields_not_in_biotools = [# first the fields unknown to bio.tools
+                          'pi', 'devstatus', 'released', 'releasedate',
+                          'affiliation',  # too complex to parse from bio.tools
+                          # doi fields contains DOIs of desired publications
+                          # to display on website. These may not be the same as
+                          # on bio.tools because of display space
+                          'primary_doi', 'uses_doi',
+                          # for downloading the tool JSON
+                          'biotools_id', 'biotools_version']
+# following fields MUST be in bio.tools entry
+biotools_required_fields = ['name', 'version', 'homepage', 'description',
+                            'operatingSystem', 'toolType']
+biotools_required_contact = ['url', 'email']  # will ok either
+
+
+stubfile = sys.argv[1]
+
+with open(stubfile) as fp:
+    stub = json.load(fp)
+output_yml = {}
+
+# check if all the "extra fields" are in there
+for field in fields_not_in_biotools:
+    try:
+        output_yml[field] = stub[field]
+    except KeyError:
+        print('Could not find field {} in stub file. Please make sure all '
+              'fields {} are in the file'.format(field, fields_not_in_biotools))
+        sys.exit(1)
+print(output_yml)
+
+# download bio.tools JSON
+tool_url = BIO_TOOLS_URL.format(output_yml['biotools_id'],
+                                output_yml['biotools_version'])
+try:
+    biotools_json = json.loads(urllib2.urlopen(tool_url).read())
+except urllib2.HTTPError:
+    print('URL {} to bio.tools did not resolve. Please make sure the tool '
+          'ID and version are correct'.format(tool_url))
+    sys.exit(1)
+except ValueError:
+    print('No correct JSON was returned when querying {}'.format(tool_url))
+    sys.exit(1)
+
+# validate fields in bio.tools JSON
+for field in biotools_required_fields:
+    try:
+        output_yml[field] = biotools_json[field]
+    except KeyError:
+        print('JSON from bio.tools does not contain the field: {}. Please make '
+              'sure all fields {} are in the '
+              'bio.tools registry'.format(field, biotools_required_fields))
+        sys.exit(1)
+    
+contact_success = False
+for field in biotools_required_contact:
+    cfield = 'contact_{}'.format(field)
+    try:
+        output_yml[cfield] = biotools_json['contact'][0][field]
+    except KeyError:
+        pass
+    else:
+        if output_yml[cfield] is not None:
+            contact_success = True
+if not contact_success:
+    print('Could not find either contactEmail or contactURL in bio.tools JSON')
+    sys.exit(1)
+
+# Get bibtex from DOIs for publications
+# curl -L -H "Accept: application/x-bibtex; charset=utf-8" http://dx.doi.org/$1
+def get_bibtex(doi):
+    url = DOI_URL.format(doi)
+    req = urllib2.Request(url, headers={'Accept': 'application/x-bibtex; charset=utf-8'})
+    return urllib2.urlopen(req).read()
+
+
+output_yml['primary_pub'] = []
+for tooldoi in output_yml['primary_doi']:
+    bibtex = get_bibtex(tooldoi)
+    bibpub = bibtexparser.loads(bibtex).entries[0]
+    article_id = bibpub['ID'].split('_')
+    output_yml['primary_pub'].append('{}, {}'.format(' '.join(article_id[:-1]),
+                                                     article_id[-1]))
+
+releasedate = output_yml.pop('releasedate')
+# write YAML
+outfile = '{}.yml'.format(stub['biotools_id'])
+with open(outfile, 'w') as fp:
+    fp.write('---\n')
+    fp.write('releasedate: {}\n'.format(releasedate))
+    yaml.safe_dump(output_yml, fp, encoding='utf-8')
+
+print('Done, wrote output to {}'.format(outfile))
